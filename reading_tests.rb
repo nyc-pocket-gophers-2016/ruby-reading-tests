4653ab2691f11e5904cff7c31fe4f8c67b1df571
Joel_L <plagueoffrogs@gmail.com>
2016-05-08 19:37:06 -0400

    Add a few more methods for tests

--- reading_tests.rb
+++ reading_tests.rb
@@ -25,3 +25,36 @@ end
 def start_with_c?(string)
   string.split.select {|word| word.start_with?('c')}.length == string.split.length
 end
+
+def titlecase(string)
+  string.split.map { |word| word.downcase.capitalize! }.join(' ')
+end
+
+def delete_last(string, range)
+  range.times  do 
+    string.chop! 
+  end
+  string
+end
+
+def to_money(float)
+  str = float.round(2).to_s
+    if str[-1] == '0' && str[-2] == '.'
+      "$#{str}0" 
+    else
+      "$#{str}"
+  end
+end
+
+def numeric?(string)
+  if string.to_s.match(/\b\d+/) != nil
+    true
+  else 
+    false
+  end
+end
+
+
+
+
+
